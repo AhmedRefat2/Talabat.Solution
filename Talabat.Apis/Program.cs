753d76ecd9129b7563e6ca4c3f10e77f63f7f4dc
Ahmed Refat <ahmed202203195@gmail.com>
2025-05-30 00:11:46 +0300

    Add project files.

---
+++ Talabat.Apis/Program.cs
@@ -0,0 +1,50 @@
+namespace Talabat.Apis
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var webApplicationBuilder = WebApplication.CreateBuilder(args);
+
+            #region Configure Services - Add The Services To DI Container 
+
+            webApplicationBuilder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            webApplicationBuilder.Services.AddEndpointsApiExplorer();
+            webApplicationBuilder.Services.AddSwaggerGen(); 
+
+            #endregion
+
+            var app = webApplicationBuilder.Build();
+
+            #region Configure - Configure The HTTP Request Pipeline
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            //app.UseRouting(); // Add routing middleware
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllerRoute(
+            //        name: "default",
+            //        pattern: "{controller}/{action=index}/{id?}");
+            //});
+
+
+            app.MapControllers(); 
+
+            #endregion
+
+            app.Run();
+        }
+    }
+}
